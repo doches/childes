73805c7cd0da12b82367a232cf86dd00be461ab9
Trevor Fountain <doches@gmail.com>
2011-02-17 15:57:41 +0000

    Fixed. And run. May never be used again.

--- tools/agemap2corpora.rb
+++ tools/agemap2corpora.rb
@@ -32,11 +32,15 @@ bins.each_pair do |age,list|
 end
 progress.finish
 
-# Concatenate into chunks (e.g. 0-6, 0-12, 0-18, ...)
-epochs = bins.map { |age,list| [age,list] }.sort { |a,b| a[0] <=> b[0] }
-epochs.each_with_index do |epoch, index|
-  progress = ProgressBar.new(epoch[0].to_s,index+1)
-  (0..index).map { |subindex| epochs[subindex][0] }.each do |bin|
-  	`cat #{File.join(output,partdir,"#{epochs[bin][0]}")} >> #{epoch[0]}.target_corpus`
-  end
+epochs = bins.keys.sort
+progress = ProgressBar.new("Concatenating",epochs.size)
+(0..epochs.size-1).each do |index|
+	p epochs[index]
+	target = File.join(output,"#{epochs[index]}.target_corpus")
+	epochs[0..index].each do |age| 
+		partial = File.join(output,partdir,"#{age}.target_corpus")
+		`cat #{partial} >> #{target}`
+	end
+	progress.inc
 end
+progress.finish
